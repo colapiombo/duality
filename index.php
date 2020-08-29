162e842de49d42508b22eea35ac046281f061f1e
colapiombo <paolo@combi.li>
2020-08-29 09:16:41 +0300

    index page and first commit for main js

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
-<?php
-declare(strict_types=1);
+<!DOCTYPE html>
+<html lang="<?php echo Theme::lang() ?>">
+<head>
+    <?php include(THEME_DIR_PHP.'head.php'); ?>
+</head>
+    <body>
 
-/**
- *
- * NOTICE OF LICENSE
- *
- * This source file is released under commercial license by Lamia Oy.
- *
- * @copyright Copyright (c) Lamia Oy (https://lamia.fi)
- *
- */
+    <!-- Load Bludit Plugins: Site Body Begin -->
+    <?php Theme::plugins('siteBodyBegin'); ?>
+
+    <!-- Navbar -->
+    <?php include(THEME_DIR_PHP.'navbar.php'); ?>
+
+
+    <?php
+    if ($url->slug() == "blog"){
+        include(THEME_DIR_PHP.'blog.php');
+    }elseif ($WHERE_AM_I == 'page') {
+        include(THEME_DIR_PHP.'page.php');
+    } elseif ( ($WHERE_AM_I=='home') )  {
+        include(THEME_DIR_PHP.'home.php');
+    }
+    ?>
+
+    <!-- Javascript -->
+    <?php
+    // Include Jquery file from Bludit Core
+    echo Theme::jquery();
+
+    // Include javascript Bootstrap file from Bludit Core
+    echo Theme::jsBootstrap();
+    echo Theme::js('js/main.js');
+
+    ?>
+    </body>
+</html>
