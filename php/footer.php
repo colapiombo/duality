f7a3300c72a7e73d7f7aa2b7216c2e1117408219
Paolo Combi <paolo@combi.li>
2020-08-31 19:41:57 +0300

    dev

---
+++ php/footer.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is released under commercial license by Lamia Oy.
+ *
+ * @copyright Copyright (c) Lamia Oy (https://lamia.fi)
+ *
+ */
