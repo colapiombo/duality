feeb629317e4182dfa47fd6f7c57561768ceabde
Paolo Combi <paolo@combi.li>
2020-09-21 10:19:09 +0300

    update post page

--- php/page.php
+++ php/page.php
@@ -8,13 +8,32 @@
             <div class="title">
                 <h2><a href="<?= $page->permalink(); ?>"><?= $page->title(); ?></a></h2>
                 <p><?= $page->description() ?></p>
-                <p><?= $L->get('Reading time') . ': ' . $page->readingTime() ?></p>
+                <ul class="tags">
+                    <?php if ($page->category() == !""): ?>
+                        <li>
+                            <a href="<?= HTML_PATH_ROOT . $url->filters('category') . '/' . strtolower($page->category()) ?>"><?= $page->category(); ?></a>
+                        </li>
+                    <?php endif ?>
 
+                    <?php foreach ($page->tags(true) as $tagKey => $tagName): ?>
+                        <li><a href="<?php echo DOMAIN_TAGS . $tagKey ?>"><?php echo $tagName; ?></a></li>
+                    <?php endforeach ?>
+                </ul>
             </div>
             <div class="meta">
-                <time class="published" datetime="<?= $page->date(); ?>"><?= $page->date(); ?></time>
-                <a href="#" class="author"><span class="name"><?= $page->user('nickname'); ?></span><img
-                            src="images/avatar.jpg" alt=""/></a>
+                <time class="published" datetime="<?= $page->date(); ?>">
+                    <?= $page->date(); ?>
+                </time>
+                <time class="published">
+                    <?= $L->get('Reading time') . ': ' . $page->readingTime() ?>
+                </time>
+                <a href="#" class="author">
+                    <span class="name">
+                        <?= $page->user('nickname'); ?>
+                    </span>
+                    <img src="<?= ($page->user('profilePicture') ? $page->user('profilePicture') : Theme::src('assets/img/favicon-32x32.png')) ?>" alt="Author"/>
+                </a>
+
             </div>
         </header>
         <!-- Cover image -->
@@ -22,57 +41,38 @@
             <span class="image featured"><img src="<?= $page->coverImage(); ?>" alt=""/></span>
         <?php endif ?>
         <?= $page->content(); ?>
+        <!-- Begin Tags -->
+        <h5>
+            Share on :
+        </h5>
+        <!-- End Tags -->
         <footer>
-            <ul class="icons">
-                <li><a href="https://twitter.com/share?text=<?php echo urlencode($page->title()) ?>&amp;url=<?php echo urlencode($page->permalink()) ?>" class="icon brands fa-twitter">
-                        <span class="label">Twitter</span>
-                    </a>
-                </li>
-                <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-                <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-                <li><a href="#" class="icon solid fa-rss"><span class="label">RSS</span></a></li>
-                <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
-            </ul>
-            <div class="share">
-                <p>Share on:</p>
-                <span class="twitter" data-url="https://twitter.com/share?text=<?php echo urlencode($page->title()) ?>&amp;url=<?php echo urlencode($page->permalink()) ?>"
-                      rel="nofollow noreferrer" title="Share on Twitter">
-                    <i class="icon-twitter"></i>
-                </span>
-                <span class="facebook"
-                                  data-url="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($page->permalink()) ?>"
-                                  data-toggle="tooltip" data-placement="top" rel="nofollow noreferrer"
-                                  data-original-title="Share on Facebook">
-                    <i class="icon-facebook"></i>
-                </span>
-                <span data-url="https://www.reddit.com/submit?url=<?php echo urlencode($page->permalink()) ?>&amp;title=<?php echo urlencode($page->title()) ?>"
-                                  data-toggle="tooltip" data-placement="top" class="reddit" rel="nofollow noreferrer"
-                                  data-original-title="Share on Reddit">
-                    <i class="icon-reddit"></i>
-                </span>
-                <a href="mailto:?subject=<?php echo rawurlencode($page->title()) ?>&amp;body=<?php echo urlencode($page->permalink()) ?>"
-                   data-toggle="tooltip" data-placement="top" class="email" rel="nofollow noreferrer"
-                   data-original-title="Share by Email">
-                    <i class="icon-envelope"></i>
-                </a>
-            </div>
-            <!-- Begin Tags -->
-            <ul class="stats">
-                <?php if ($page->category() == !""): ?>
+                <ul class="icons">
                     <li>
-                        <a href="<?= HTML_PATH_ROOT . $url->filters('category') . '/' . strtolower($page->category()) ?>"><?= $page->category(); ?></a>
+                        <a href="https://twitter.com/intent/tweet?text=<?php echo urlencode($page->title()) ?>&amp;url=<?php echo urlencode($page->permalink()) ?>" class="icon brands fa-twitter" title="Share on Twitter" target="_blank">
+                            <span class="label">Twitter</span>
+                        </a>
                     </li>
-                <?php endif ?>
-
-                <?php foreach ($page->tags(true) as $tagKey => $tagName): ?>
-                    <li><a href="<?php echo DOMAIN_TAGS . $tagKey ?>"><?php echo $tagName; ?></a></li>
-                <?php endforeach ?>
-            </ul>
-            <!-- End Tags -->
+                    <li>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($page->permalink()) ?>" class="icon brands fa-facebook-f"  title="Share on Facebook" target="_blank">
+                            <span class="label">Facebook</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.reddit.com/submit?url=<?php echo urlencode($page->permalink()) ?>&amp;title=<?php echo urlencode($page->title()) ?>" class="icon brands fa-reddit" title="Share on Reddit" target="_blank">
+                            <span class="label">Rettid</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:?subject=<?php echo rawurlencode($page->title()) ?>&amp;body=<?php echo urlencode($page->permalink()) ?>" class="icon solid fa-envelope" title="Share by Email" target="_blank">
+                            <span class="label">Email</span>
+                        </a>
+                    </li>
+                </ul>
         </footer>
-
     </article>
 </div>
+<?php  include(THEME_DIR_PHP . 'footer.php');  ?>
 
 <!-- Load Bludit Plugins: Page End -->
 <?php Theme::plugins('pageEnd'); ?>
